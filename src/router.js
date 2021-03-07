a0861f4941b5dd08453275b235e054534c3f6c00
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2021-03-06 23:58:28 -0300

    Update

---
+++ src/router.js
@@ -0,0 +1,19 @@
+// import 'react-native-gesture-handler';
+
+import { NavigationContainer, createStackNavigator } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './page1.js';
+import Profile from './page2.js';
+
+const Stack = createStackNavigator();
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Main />
+            <Profile />
+        </NavigationContainer>
+    );
+};
+export default Routes;
